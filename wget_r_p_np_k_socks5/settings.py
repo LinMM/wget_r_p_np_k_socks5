85f85f5d0d16fc6aeb96a17adfa9390ce2007dc5
LinMM <lmmv587@gmail.com>
2013-08-26 19:50:14 +0800

    hello

---
+++ wget_r_p_np_k_socks5/settings.py
@@ -0,0 +1,15 @@
+# Scrapy settings for wget_r_p_np_k_socks5 project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/topics/settings.html
+#
+
+BOT_NAME = 'wget_r_p_np_k_socks5'
+
+SPIDER_MODULES = ['wget_r_p_np_k_socks5.spiders']
+NEWSPIDER_MODULE = 'wget_r_p_np_k_socks5.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'wget_r_p_np_k_socks5 (+http://www.yourdomain.com)'
